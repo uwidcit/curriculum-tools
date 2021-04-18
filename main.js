93b4eff894900aa3a671b014e8fbee773343c0c6
Snickdx <snickdx@gmail.com>
2021-04-18 12:18:10 -0400

    it domains

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const {parse} = require('./parser');
 
 async function main(){
-    const file = await fs.readFileSync('file.pdf');
-    const text = await parse(file);
-    await fs.writeFileSync('output.json', JSON.stringify(text, null, 2));
+    const file = await fs.readFileSync('it-topics.pdf');
+    const text = await parse(file, 'IT');
+    await fs.writeFileSync('it-output.json', JSON.stringify(text, null, 2));
     console.log('dumped');
 }   
 
--- parser.js
+++ parser.js
@@ -24,10 +24,13 @@ const domains = {
     'SF':'System Fundamentals'
 };
 
+const ITdomains = {
+    'CSP':'Cybersecurity Principles'
+}
+
 const HEADINGSIZE = 18.04;
 const TOPICSIZE1 = 12.96;
 const TOPICSIZE2 = 14.04;
-const H2SIZE = 13.96;
 const TIERSIZE = 12.96;
 const CATSIZE = 17.04;
 
@@ -40,6 +43,29 @@ function getTier(text){
     if(tier1) return 'Tier 1';
     if(tier2) return 'Tier 2';
     return 'Both';
+}
+
+function getObj(text, run){
+    return {
+        y: text.y,
+        x: text.x,
+        text: decode(run["T"]).trim(),
+        size: run["TS"][1],
+        bold: run["TS"][2] === 1,
+        italic: run["TS"][3] === 1
+    };
+}
+
+function isDomain(string){
+    return ((string.startsWith("ITE") || string.startsWith("ITS")) && string.split('-').length === 2 )
+}
+
+function isSubDomain(string){
+    return ((string.startsWith("ITE") || string.startsWith("ITS")) && string.split('-').length === 3 )
+}
+
+function isTopics(string){
+    return !(string.startsWith("ITE") || string.startsWith("ITS"))
 }
 
 /**
@@ -58,38 +84,25 @@ async function getPDFText(fileBuffer){
 
     for(let page of json['formImage']['Pages']){
         for(let text of page['Texts']){
-            const runs = text["R"];
-            const textObjs = runs.map(run=>{
-                return {
-                    y: text.y,
-                    x: text.x,
-                    text: decode(run["T"]),
-                    size: run["TS"][1],
-                    bold: run["TS"][2] === 1,
-                    italic: run["TS"][3] === 1
-                }
-            });
-
-            pdfText.push(...textObjs);
+            const runs = text["R"].map(item => getObj(text, item));
+            pdfText.push(runs[0]);
         }
     }
 
     let [prev, ...rest] = pdfText;
-    const merged = [];
+    let merged = [];
 
-    for(let obj of rest){
-        if( prev.y === obj.y ){
-            prev.text+=obj.text;
+    for(let text of rest){
+        if(text.y === prev.y){
+            prev.text+=text.text;
         }else{
-
             merged.push(prev);
-            prev = obj;
+            prev=text;
         }
-     
-    }
-    
-    return merged;
 
+    }
+
+    return merged;
 }
 
 function isSubdomain(text){
@@ -97,8 +110,18 @@ function isSubdomain(text){
     return text.size === HEADINGSIZE && text.text.includes('/');
 }
 
+function getSubDomainIT(){
 
-function getTopics(texts){
+}
+
+function getDomainIT(){
+
+}
+
+function getTopicsIT(texts){
+  
+
+
     const topics = [];
        
     let i = 0;
@@ -160,6 +183,67 @@ function getTopics(texts){
 }
 
 
+function getTopicsCS(texts){
+    const topics = [];
+       
+    let i = 0;
+    let cursub = null;
+    let subcounter = 0;
+
+
+    while(i < texts.length){
+        while(!isSubdomain(texts[i]))i++;
+  
+        cursub = texts[i];
+        const [domainId, subdomain] = cursub.text.split('/');
+        topics[cursub.text] = [];
+        let counter = 1;
+        subcounter++;
+
+        i++;
+
+        let tier="N/A";
+        let breakdown = null;
+        
+        while(i < texts.length && !isSubdomain(texts[i]) ){
+
+            if(texts[i].size === CATSIZE )
+                breakdown = texts[i].text; 
+            
+            if(texts[i].size === TIERSIZE && texts[i].x > 4 && texts[i].x < 4.5){
+                tier = getTier(texts[i].text)
+                if(tier === "Both"){
+                    console.log(texts[i].text);
+                    tier = 'N/A';
+                }
+            }
+                 
+
+            if( texts[i].text[0] === "•" && i < texts.length && (texts[i].size === TOPICSIZE1 || texts[i].size === TOPICSIZE2 )){
+                text = texts[i].text.replace("•", "");
+                const subdomainId = `${domainId}-${subcounter.toString().padStart(2, '0')}`;
+                const topicId = `${subdomainId}-${counter.toString().padStart(2, '0')}`;
+                if(tier === "N/A")tier = getTier(breakdown);
+                topics.push({
+                    breakdown,
+                    topic: text,
+                    domainId,
+                    domain: domains[domainId],
+                    subdomain,
+                    subdomainId,
+                    topicId,
+                    tier: tier,
+                });
+                counter++;
+            }
+            i++;
+        }
+
+    }
+
+    return topics;
+}
+
 function getSubdomains(){
     const subs = [];
     for(let sub of subdomains){
@@ -169,9 +253,10 @@ function getSubdomains(){
 }
 
 
-async function parse(file){
+async function parse(file, prog){
     const text = await getPDFText(file);
-    return topics = getTopics(text);
+    return topics = (prog === "CS") ? getTopicsCS(text) : getTopicsIT(text);
+    return topics;
 }
 
 
