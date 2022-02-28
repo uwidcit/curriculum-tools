4010ccf31a94863e0f25c66148b8a0ea383a28f2
Kris Manohar <justkrismanohar@gmail.com>
2022-02-28 16:56:13 -0400

    Fixed character set issues. Using actual template.docx

--- main-render.js
+++ main-render.js
@@ -39,37 +39,25 @@ async function main(){
     }
     */
 
+    //Word saves files using windows-1252 character set
+    //which is based on latin1 
+    //https://stackoverflow.com/questions/62557890/reading-a-windows-1252-file-in-node-js
     
-    fs.readFile("./render_template/test4.htm", (error, data) => {
+    fs.readFile("./render_template/Template.htm", 'latin1', (error, data) => {
         if(error) {
             throw error;
         }
-        //console.log(data.toString());
+        
         var out = data.toString().replace(/@@@tableHTML/,res);
-        //var out = data.toString().replace(/@@@tableHTML/,"HELLO");
-        //var out = data.toString();
-
+        
         try {
-            fs.writeFileSync('./render_output/test4.htm', out, 'utf-8');
+            fs.writeFileSync('./render_output/Template.htm', out, 'latin1');
             //file written successfully
         } catch (err) {
             console.error(err)
         }
 
     });
-
-
-    /*
-    const res = await render();
-    //writeJSON('data/parsed-topics.json', res);
-    //console.log(res);
-    try {
-        fs.writeFileSync('./render_output/table.txt', res)
-        //file written successfully
-    } catch (err) {
-        console.error(err)
-    }
-    */
 }
 
 main().catch(console.error);
