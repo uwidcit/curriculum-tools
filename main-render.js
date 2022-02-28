57000547bf5b3a3128f2aefee1f310ee6523c243
Kris Manohar <justkrismanohar@gmail.com>
2022-02-28 16:36:12 -0400

    relances @@@tableHTML in template file with table HTML

--- main-render.js
+++ main-render.js
@@ -3,11 +3,63 @@ const fs = require('fs')
 
 const {writeJSON} = require('./util');
 
-
-
 async function main(){
     
+    /*
 
+        VS-Code or JS doing something weird with this text
+
+        <span style='mso-spacerun:yes'>  </span><o:p></o:p></span>
+
+        when saveing as a template literal string....
+        It rpelaces with garbage values...
+    */
+    const res = await render();
+
+    /*
+    try {
+        fs.writeFileSync('./render_output/course.htm', before_table_HTML, 'utf-8');
+        //file written successfully
+    } catch (err) {
+        console.error(err);
+    }
+
+    try {
+        fs.appendFileSync('./render_output/course.htm', res, 'utf-8');
+        //file written successfully
+    } catch (err) {
+        console.error(err);
+    }
+    
+    try {
+        fs.appendFileSync('./render_output/course.htm', after_table_HTML, 'utf-8');
+        //file written successfully
+    } catch (err) {
+        console.error(err);
+    }
+    */
+
+    
+    fs.readFile("./render_template/test4.htm", (error, data) => {
+        if(error) {
+            throw error;
+        }
+        //console.log(data.toString());
+        var out = data.toString().replace(/@@@tableHTML/,res);
+        //var out = data.toString().replace(/@@@tableHTML/,"HELLO");
+        //var out = data.toString();
+
+        try {
+            fs.writeFileSync('./render_output/test4.htm', out, 'utf-8');
+            //file written successfully
+        } catch (err) {
+            console.error(err)
+        }
+
+    });
+
+
+    /*
     const res = await render();
     //writeJSON('data/parsed-topics.json', res);
     //console.log(res);
@@ -17,7 +69,7 @@ async function main(){
     } catch (err) {
         console.error(err)
     }
-    
+    */
 }
 
 main().catch(console.error);
